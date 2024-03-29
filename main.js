78fd9dbb0fa9f0e514c3b1d1d54ed40bfde2c4db
saungnaga <saungnagaa@gmail.com>
2024-03-29 23:50:05 +0700

    fixing counter bug

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-const animals = ["Frog","Crane","Rabbit","Cricket","Tiger"]
+const animals = ["Frog","Flamingo","Rabbit","Cricket","Tiger"]
 
 let animalDetails = {
     "Frog": "LAME!!! frog is so lame and disgusting 🤮 :(",
-    "Crane": "your soul is beautiful just like a crane 😍",
+    "Flamingo": "your soul is beautiful just like a Flamingo 😍",
     "Rabbit": "Clever, rabbit is very clever and fast! ⚡",
     "Cricket": "you're annoying, just like a noisy cricket 😒", "Tiger": "Brave! but tiger is kind of psychopath 🐯 haha"
 }
 
 let animalFound = 0;
+
+let firstClick = true;
 
 function getAnimals() {
     // Generate a random index within the range of the array length
@@ -22,5 +24,11 @@ function getAnimals() {
     //animal counter
     animalFound++;
     let animalCounter = document.getElementById("animalCounter")
-    animalCounter.textContent = "seems you don't like your first soulmate 😔 you've change your soul animal for " + animalFound + " time(s)"
+    
+    if (firstClick) {
+        animalCounter.textContent = "okay, your animal is " + randomAnimals + ", you should take it for the rest of your life!";
+        firstClick = false;
+    } else {
+        animalCounter.textContent = "seems you don't like your first soulmate 😔 you've change your soul animal for " + (animalFound-1) + " time(s)"
+    }
 }
